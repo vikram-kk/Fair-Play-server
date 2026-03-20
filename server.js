234a36ab2a79a8fe43d1221648a89a4ef931b670
Vikram Thakur <vikramkk380@gmail.com>
2026-03-20 22:11:21 +0530

    db fix again

--- server.js
+++ server.js
@@ -43,5 +43,5 @@ app.use('/subscription', subscriptionRoute)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server started at http://localhost:${PORT}`);
+    console.log(`Server started at port http://localhost:${PORT}`);
 });
